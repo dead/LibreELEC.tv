536a44138748e7d329744038518840e6d159e1d1
Gustavo <gustavobenn@gmail.com>
2019-12-20 11:39:02 -0300

    fix moonlight

---
+++ packages/addons/script/moonlight/depends/moonlight-common-c/package.mk
@@ -0,0 +1,30 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="moonlight-common-c"
+PKG_VERSION="e60a7ef"
+PKG_SHA256="4be76beefcb96f2bf56f07f8b09a78271dbe3635a51f68274b790562e1d0fe9c"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/moonlight-stream/moonlight-common-c"
+PKG_URL="https://github.com/moonlight-stream/moonlight-common-c/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION=""
+PKG_SHORTDESC="Shared C code for the implementation of Nvidia's GameStream protocol"
+PKG_LONGDESC="Shared C code for the implementation of Nvidia's GameStream protocol"
+PKG_TOOLCHAIN="manual"
